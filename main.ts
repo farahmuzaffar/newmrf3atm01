4ca7806fa58d6da007751ffc7093ab61ef0120b4
farahmuzaffar <164481851+farahmuzaffar@users.noreply.github.com>
2024-04-22 21:45:48 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; //Dollar
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+  },
+]);
+if (pinAnswer.pin === myPin) {
+  console.log("correct pin code!!!");
+  let accountType = await inquirer.prompt([
+    {
+      name: "account",
+      message: "enter your account type",
+      type: "list",
+      choices: ["savings account", "current account"],
+    },
+  ]);
+  if (accountType.account === "savings account") {
+  } else if (accountType.account === "current account") {
+  }
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "please select option",
+      type: "list",
+      choices: ["withdraw", "check balance"],
+    },
+  ]);
+
+  console.log(operationAns);
+  if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "please enter your amount",
+        type: "number",
+      },
+    ]);
+    myBalance -= amountAns.amount;
+    console.log("your remaining balance is" + myBalance);
+  } else if (operationAns.operation === "check balance") {
+    console.log("your balance is" + myBalance);
+  }
+} else {
+  console.log("incorrect pin code");
+  console.log("please try again");
+}
